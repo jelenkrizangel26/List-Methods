0d0e77f7eaffaa7f869c86a86bd21d5fce624ea3
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-11-05 11:51:04 +0800

    Done including the pick, will just edit to do the commands

--- testingList_Methods.py
+++ testingList_Methods.py
@@ -14,25 +14,78 @@
 #- Record a demo presenting your program
 #- Send the demo directly to my messenger
 
+import sys
+
+list1 = [10, 40, 20, 60, 30, 50, 70, 80, 100, 200]
+border = ("\33[35m******************************************\33[0m\n")
+
 def intro():
     print("Good day, Welcome to List Manipulation!")
     print("So here's our list for today.\n")
 
 def list():
-    border = ("\33[35m******************************************\33[0m\n")
-    list1 = [10, 40, 20, 60, 30, 50, 70, 80, 100, 200]
     print(border)
     print(list1)
     print(border)
 
 def menu():
     list2 = ["1 -> Add an element" ,"2 -> Insert an element", "3 -> Modify an element" ,"4 -> Delete an element" ,"5 -> Arrange in ascending order" ,"6 -> Arrange in descending order"]
+    print("Instructions: Pick number from 1 to 6 to tell what you want. \n Menu:")
     print(list2[0])
     print(list2[1])
     print(list2[2])
     print(list2[3])
     print(list2[4])
     print(list2[5])
+
+def start():
+
+    while True:
+        print(border)
+        pick = input("What do you want to do ? \n>>")
+        if pick == '1':
+            print(border)
+            string = "\n\t              \33[3m\33[1m\33[35m Starting!\33[0m" 
+            print(string)
+            number = input("Enter the number you want to add: \n >>")
+            list1.append(number)
+            print(list1)
+            sys.exit("\n")
+
+        elif pick == '2':
+            string = "\n\t              \33[3m\33[1m\33[35m Starting!\33[0m" 
+            print(string)
+            sys.exit("\n")
+
+        elif pick == '3':
+            string = "\n\t              \33[3m\33[1m\33[35m Starting!\33[0m" 
+            print(string)
+            sys.exit("\n")
+
+        elif pick == '4':
+            string = "\n\t              \33[3m\33[1m\33[35m Starting!\33[0m" 
+            print(string)
+            sys.exit("\n")
+
+        elif pick == '5':
+            string = "\n\t              \33[3m\33[1m\33[35m Starting!\33[0m" 
+            print(string)
+            sys.exit("\n")
+
+        elif pick == '6':
+            string = "\n\t              \33[3m\33[1m\33[35m Starting!\33[0m" 
+            print(string)
+            sys.exit("\n")
+        
+
+
+
+
+
+        
+
+
+
     
     
 
@@ -40,3 +93,4 @@ def menu():
 intro()
 list()
 menu()
+start()
